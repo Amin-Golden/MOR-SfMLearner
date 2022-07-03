53047a83952fa6895a57432e0d12677a59cfca83
Amin-Golden <aminabazari@gmail.com>
2022-07-03 13:48:26 +0430

    modify Path

--- test_mor_vo.py
+++ test_mor_vo.py
@@ -20,13 +20,14 @@ import os
 import sys
 
 # yolov5 
-from pathlib import Path1
+from pathlib import Path as Path1
 from models.common import DetectMultiBackend
 from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
 from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
                            increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, time_sync
+
 FILE = Path1(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
 if str(ROOT) not in sys.path1:
