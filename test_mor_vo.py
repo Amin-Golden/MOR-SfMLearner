b9cb1581167aa8726c7e447ccf09283411298c34
Amin-Golden <aminabazari@gmail.com>
2022-07-03 13:45:35 +0430

    modify path

--- test_mor_vo.py
+++ test_mor_vo.py
@@ -20,18 +20,18 @@ import os
 import sys
 
 # yolov5 
-from pathlib import Path
+from pathlib import Path1
 from models.common import DetectMultiBackend
 from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
 from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
                            increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
 from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, time_sync
-FILE = Path(__file__).resolve()
+FILE = Path1(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
-if str(ROOT) not in sys.path:
-    sys.path.append(str(ROOT))  # add ROOT to PATH
-ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+if str(ROOT) not in sys.path1:
+    sys.path1.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path1(os.path1.relpath(ROOT, Path1.cwd()))  # relative
 
 parser = argparse.ArgumentParser(description='Script for visualizing depth map and masks',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
