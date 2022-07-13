fa4fe04025accf85b64e566aa8253b1b4e9c36c8
Amin-Golden <aminabazari@gmail.com>
2022-07-13 19:28:34 +0430

    remove gt

--- scripts/train_resnet50_pose_256.sh
+++ scripts/train_resnet50_pose_256.sh
@@ -3,11 +3,11 @@ TRAIN_SET=Dataset/kitti_vo_256/
 python train.py $TRAIN_SET \
 --resnet-layers 50 \
 --num-scales 1 \
--b4 -s0.1 -c0.5 --epochs 10 --epoch-size 10 --sequence-length 3 \
+-b4 -s0.1 -c0.5 --epochs 10 --epoch-size 1000 --sequence-length 3 \
 --with-ssim 1 \
 --with-mask 0 \
 --with-auto-mask 1 \
 --with-pretrain 1 \
 --pretrained-pose models/exp_pose_model_best40.pth.tar \
---log-output --with-gt \
+--log-output \
 --name resnet50_pose_256
