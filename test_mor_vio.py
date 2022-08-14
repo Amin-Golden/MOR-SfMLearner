b0e34468348e78dcf35b757ea294742f1cec48e6
Amin-Golden <aminabazari@gmail.com>
2022-08-14 18:52:22 +0430

    a_check

--- test_mor_vio.py
+++ test_mor_vio.py
@@ -185,7 +185,7 @@ def main():
 
     #### 4. Measurement Update #####################################################################
 
-    def measurement_update(sensor_var, p_cov_check, y_k, p_check, v_check, q_check):
+    def measurement_update(sensor_var, p_cov_check, y_k, p_check, v_check, q_check,a_check):
         # Compute Kalman Gain
         R = np.diag([sensor_var, sensor_var, sensor_var]) # Measurement covariance matrix calculation
 
