691989666b0b7d7cf682f8277b795b3eb82d42dd
Amin-Golden <aminabazari@gmail.com>
2022-08-17 01:13:25 +0430

    q

--- test_mor_vio.py
+++ test_mor_vio.py
@@ -366,7 +366,7 @@ def main():
             # global_traj = current_pose.dot(pose_mat)      
             # print("pose_mat",pose_mat)
             # tarj = np.linalg.inv(pose_mat)
-            trajectory = (Rc0.dot(global_traj[0:3,3])).T #
+            trajectory = ((global_traj[0:3,3])).T #Rc0.dot
            
             delta_t = 0.1 # time_s[k - 1]-time_s[k]
             
